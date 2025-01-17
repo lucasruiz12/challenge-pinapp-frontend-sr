eebd1bc327ededc714e08fdcb9e2da449f13bc39
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-01-17 14:40:55 -0300

    adding configurations in eslint to deploy

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",  // Desactiva la regla para variables no utilizadas
+    },
+  },
 ];
 
 export default eslintConfig;
